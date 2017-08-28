f0e8677f390445776996ccc5b531dfb4edd8f67f
Alex Dontsov <dcorpsoftware@gmail.com>
2017-08-28 09:28:53 +0700

    add geoobjects

--- app/shapeEditor/admin.py
+++ app/shapeEditor/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib.gis import admin
+from models import *
 
-# Register your models here.
+admin.site.register(Shapefile, admin.ModelAdmin)
+admin.site.register(Feature, admin.GeoModelAdmin)
+admin.site.register(Attribute , admin.ModelAdmin)
+admin.site.register(AttributeValue , admin.ModelAdmin)
--- app/shapeEditor/models.py
+++ app/shapeEditor/models.py
@@ -14,3 +14,21 @@ class Attribute(models.Model):
     type = models.IntegerField()
     width = models.IntegerField()
     precision = models.IntegerField()
+
+class Feature(models.Model):
+    shapefile =models.ForeignKey(Shapefile)
+    geom_point = models.PointField(srid=4326, blank=True, null=True)
+    geom_multipoint =models.MultiPointField(srid=4326, blank=True, null=True)
+    geom_multilinestring = models.MultiLineStringField(srid=4326, blank=True, null=True)
+    geom_multilipolygon = models.MultiPolygonField(srid=4326, blank=True, null=True)
+    geom_geometrycolletion = models.GeometryCollectionField(srid=4326, blank=True, null=True)
+
+    objects = models.GeoManager()
+
+class AttributeValue(models.Model):
+    feature = models.ForeignKey(Feature)
+    attribute = models.ForeignKey(Attribute)
+    value = models.CharField(max_length=255, blank=True, null=True)
+
+
+
