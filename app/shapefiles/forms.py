d198b26c6afc42c84e1b6c9dced21f955cdacbe5
Alex Dontsov <dcorpsoftware@gmail.com>
2017-09-20 22:24:44 +0700

    add form

---
+++ app/shapefiles/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class ImportShapefileForm(forms.Form):
+    import_file = forms.FileField(label="Bbi6epHTe сжатый файл фигур")
--- app/shapefiles/views.py
+++ app/shapefiles/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.shapeEditor.models import Shapefile
+from django.http import HttpResponseRedirect
+from forms import ImportShapefileForm
 
 def list_shapefiles (request):
     shapefiles = Shapefile.objects.all().order_by('filename')
@@ -11,4 +13,13 @@ def list_shapefiles (request):
                   { 'shapefiles' : shapefiles })
 
 def import_shapefile (request):
-    return HttpResponse("Продолжение с л ед у е т ..." )
+    if request.method == "GET":
+        form = ImportShapefileForm()
+        return render(request, "import_shapefile.html", {'form': form})
+    elif request.method == "POST":
+        form = ImportShapefileForm(request.POST, request.FILES)
+        if form. is_valid():
+            shapefile = request.FILES['import_file']
+            # Продолжение с л е д у е т ...
+            return HttpResponseRedirect("/")
+        return render(request, "import_shapefile.html", {'form' : form})
