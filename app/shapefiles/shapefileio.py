573bb1b61c09022a3d342eb2314d5ec66b937d9d
Alex Dontsov <dcorpsoftware@gmail.com>
2017-10-09 21:57:52 +0700

    as

---
+++ app/shapefiles/shapefileio.py
@@ -0,0 +1,2 @@
+def import_data(shapefile):
+    return "Продолжение следует. . . "
--- app/shapefiles/views.py
+++ app/shapefiles/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from app.shapeEditor.models import Shapefile
 from django.http import HttpResponseRedirect
 from forms import ImportShapefileForm
+from django.shortcuts import render_to_response
+from shapeEditor.shapefiles import shapefilelO
 
 def list_shapefiles (request):
     shapefiles = Shapefile.objects.all().order_by('filename')
@@ -20,6 +22,9 @@ def import_shapefile (request):
         form = ImportShapefileForm(request.POST, request.FILES)
         if form. is_valid():
             shapefile = request.FILES['import_file']
+            errMsg = shapefileIO.import_data(shapefile)
+            if errMsg == None:
+                return HttpResponseRedirect('/')
             # Продолжение с л е д у е т ...
             return HttpResponseRedirect("/")
         return render(request, "import_shapefile.html", {'form' : form})
