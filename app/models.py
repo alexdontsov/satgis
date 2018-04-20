cfbb9ae10a52b8405b8ca1f718cb4e6bdc831cb9
Alex Dontsov <dontsov@sibers.com>
2018-04-20 17:19:18 +0700

    object

--- app/models.py
+++ app/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class WaterObject(models.Model):
+    title = models.CharField(max_length=255)
+    lat = models.CharField(max_length=255)
+    long = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.filename
--- getData/models.py
+++ getData/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-
-from raster.models import RasterLayer
+# from django.db import models
+#
+# from raster.models import RasterLayer
 
 # Create your models here.
