a03e3fe69cb958bf5880d64f6f9f9e0fa9f724df
Alex Dontsov <dcorpsoftware@gmail.com>
2018-09-26 09:16:15 +0700

    sd

--- getData/Sentinel.py
+++ getData/Sentinel.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import datetime
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from app.models import RasterData, WaterObject
+from app.models import RasterData, WaterObject, RasterLayer
 import zipfile
 import os
 
@@ -45,7 +45,7 @@ def getSentinelData(geojson, waterObject, date='NOW-2DAYS', platformname='Sentin
             print product['ingestiondate']
             # exit()
             p = RasterData(title=product['title'], product_id=product_id, waterObject=waterObj,
-                           date=datetime.datetime(product['ingestiondate'],  tzinfo=pytz.UTC))
+                           date=product['ingestiondate'])
             p.save()
             zip = zipfile.ZipFile(product['title'] + '.zip')
             zip.extractall('./rasters')
@@ -61,10 +61,19 @@ def getSentinelData(geojson, waterObject, date='NOW-2DAYS', platformname='Sentin
             os.makedirs('./rasters/' + product['title'])
             for file in files:
                 if 'xml' not in file:
-                    ds = gdal.Open(patch + file)
-                    ds = gdal.Translate('./rasters/' + product['title'] + '/' + file, ds,
-                                        projWin=[571377.923077, 6067937.5, 609733.615385, 6021562.5])
-                    ds = None
+
+                    # ds = gdal.Open(patch + file)
+                    # ds = gdal.Translate('./rasters/' + product['title'] + '/' + file, ds,
+                    #                     projWin=[571377.923077, 6067937.5, 609733.615385, 6021562.5])
+                    # ds = None
+
+                    print 'gdal_translate -of GTiff -a_nodata 0 -projwin 571377.923077, 6067937.5, 609733.615385, 6021562.5   ' + patch + file + ' ./rasters/' + product['title'] + '/' + file
+
+                    os.system(
+                        'gdal_translate -of GTiff -a_nodata 0 -projwin 571377.923077, 6067937.5, 609733.615385, 6021562.5  '
+                        + patch + file
+                        + ' ./rasters/' + product['title'] + '/' + file)
+
                     '''
                     /home/alex/DCORP/SatGis/satgis/rasters/S2A_MSIL1C_20180819T054641_N0206_R048_T44UNF_20180819T075215/
                     T44UNF_20180816T053641_B02.jp2 /home/alex/DCORP/SatGis/satgis/rasters/S2A_MSIL1C_20180819T054641_N0206_R048_T44UNF_20180819T075215/b1.tif
@@ -77,7 +86,7 @@ def getSentinelData(geojson, waterObject, date='NOW-2DAYS', platformname='Sentin
                     satgis/rasters/S2A_MSIL1C_20180816T053641_N0206_R005_T44UNF_20180816T083341/test.tif
                     '''
                     os.system(
-                        'gdalwarp -overwrite -s_srs EPSG:32644 -t_srs EPSG:4326 -of GTiff ' + './rasters/' + product['title'] + '/' + file
+                        'gdalwarp  -overwrite -s_srs EPSG:32644 -t_srs EPSG:4326 -of GTiff ' + './rasters/' + product['title'] + '/' + file
                         + '  ./rasters/' + product['title'] + '/' + transform_file)
 
                     # delete jp2
@@ -86,8 +95,9 @@ def getSentinelData(geojson, waterObject, date='NOW-2DAYS', platformname='Sentin
             print 'calc NDWI indes for ' + product['title']
             indexNDWI(product)
             ############
-            l = RasterData(title=product['title'], product_id=product_id, waterObject=waterObj,
-                           date=datetime.datetime(product['ingestiondate'], tzinfo=pytz.UTC))
+            l = RasterLayer(title=product['title'], product_id=product_id, waterObject=waterObj,
+                           date=product['ingestiondate'],
+                           file=product['title'] + '/' + 'ndwi.tif')
             l.save()
 
             # delete data
@@ -99,12 +109,16 @@ def getSentinelData(geojson, waterObject, date='NOW-2DAYS', platformname='Sentin
     # api.download('d833db51-85fe-4242-9bb2-e7b129d0b117')
     # api.to_geodataframe(products)
 
-
+'''
+("T44UPF_20180918T054631_B04@1"-"T44UPF_20180918T054631_B08@1")/("T44UPF_20180918T054631_B04@1"+"T44UPF_20180918T054631_B08@1")
+'''
 def indexNDWI(product):
     command = 'gdal_calc.py -A ./rasters/' + product['title'] + '/*_B04.tif ' \
-                  '-B ./rasters/' \
-              + product['title'] + '/*_B08.tif --outfile=./rasters/' + product['title'] + '/' + 'ndwi.tif --calc="(A-B)/(A+B)"'
+                  '-B ./rasters/' + product['title'] + '/*_B08.tif --outfile=./rasters/'\
+              + product['title'] + '/' + 'ndwi.tif --calc="(A-B)/(A+B)"'
+    print command
     os.system(command)
+
 
 def createComposite(product):
     command = 'gdal_calc.py -A ./rasters/' + product['title'] + '/*_B04.tif ' \
