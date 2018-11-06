6bf95c52ed0e325fd21a375ac050519bb461d5af
Alex Dontsov <dcorpsoftware@gmail.com>
2018-11-06 12:23:53 +0700

    as

--- app/views.py
+++ app/views.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response, get_object_or_404
 from .models import WaterObject, Article, Metering, RasterLayer
+from django.conf import settings
+
+
 
 
 def all_waterobjects(request):
@@ -15,9 +18,9 @@ def one_waterobject_by_slug(request, slug):
     layers = RasterLayer.objects.select_related().filter(waterObject=article.id).order_by('-date')
 
     #if meterings:
-    #    return render(request, 'water_obj_data.html', {'article': article, 'meterings': meterings})
-
-    return render(request, 'water_obj.html', { 'article': article, 'layers': layers })
+    #    return render(request, 'water_obj_data.html', {'article': article, 'meterings': meterings,})
+    project_path = settings.BASE_DIR
+    return render(request, 'water_obj.html', { 'article': article, 'layers': layers, 'project_path': project_path })
 
 def all_articles(request):
     articles = Article.objects.all()
