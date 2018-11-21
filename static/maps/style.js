14c67573878216712a733f16d7f2bb1ce6731d0a
Alex Dontsov <dcorpsoftware@gmail.com>
2018-11-21 11:03:57 +0700

    sa

--- static/maps/style.js
+++ static/maps/style.js
@@ -1,12 +1,6 @@
 function styleitem() {
   var cls = "CLASS";
 
-  //Make symbol size 14 or 7
-  var size = shape.attributes.NAME.length > 10 ? 14:7;
-
-  var style1 = "STYLE SIZE " + size + " SYMBOL 'circle'";
-  var style2 = "STYLE SIZE " + size + " SYMBOL 'cross'";
-
   var red = Math.random()*255;
   var green = Math.random()*255;
   var blue = Math.random()*255;
